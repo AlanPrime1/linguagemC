d562fa8f89facef42674a89c402fdf0c0e154c48
AlanPrime1 <128154118+AlanPrime1@users.noreply.github.com>
2023-05-23 11:08:22 -0300

    Create olamundo.c

---
+++ olamundo.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main() {
+
+int idade = 10;
+float peso= 55;
+char nome[100]="Alan";
+char sexo='f';
+
+printf("idade: %d \n", idade);
+
+printf("peso: %.2f \n", peso);
+
+printf("nome: %s \n", nome);
+printf("sexo: %c \n", sexo); 
+
+system("pause");
+
+return 0;
+}
