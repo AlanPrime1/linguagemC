b015914a91c8f8f5c5ecd17a1cbbc1776e86fcbd
AlanPrime1 <128154118+AlanPrime1@users.noreply.github.com>
2023-05-23 11:09:57 -0300

    Create condicional.c

---
+++ condicional.c
@@ -0,0 +1,34 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main() {
+
+
+    float nota1, nota2, media;
+    
+
+    
+
+    printf("Digite a primeira nota:");
+    scanf("%f", & nota1);
+    printf("Digite a segunda nota:");
+    scanf("%f", & nota2);
+
+    media = (nota1+nota2)/2;
+
+
+    if (media >= 7) {
+
+        printf("Aprovado");
+    } else{
+
+        printf("Reprovado");
+    }
+
+
+    
+    
+
+
+    return 0;
+}
